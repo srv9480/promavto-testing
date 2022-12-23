4f604193109e7968424f9f468d10ca9f5c3b6da6
srv9480 <102672439+srv9480@users.noreply.github.com>
2022-12-23 15:23:00 +0300

    ADD чекбокс с обработчиком и отслеживанием состояния используя хук react useState

--- src/components/CheckBox/index.jsx
+++ src/components/CheckBox/index.jsx
@@ -1,10 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import './styles.css';
 
 export default function Check() {
+    const [checkedOne, setCheckedOne] = useState(false);
+    const [checkedTwo, setCheckedTwo] = useState(false);
+
+    const handleChangeOne = () => {
+        setCheckedOne(!checkedOne);
+    };
+
+    const handleChangeTwo = () => {
+        setCheckedTwo(!checkedTwo);
+    };
     return (
-        <div>
-            
+        <div className="check-box">
+            <h1>Чек бокс</h1>
+            <div className="check-box__flex-container">
+                <Checkbox
+                    label="Значение 1"
+                    value={checkedOne}
+                    onChange={handleChangeOne}
+                    styles={{cursor: 'pointer'}}
+                />
+                <Checkbox
+                    label="Значение 2"
+                    value={checkedTwo}
+                    onChange={handleChangeTwo}
+                />
+            </div>
         </div>
+    );
+};
+
+const Checkbox = ({ label, value, onChange }) => {
+    return (
+        <label>
+            <input type="checkbox" checked={value} onChange={onChange} />
+            {label}
+        </label>
     );
 };
