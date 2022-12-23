341ef3c8d894b366840e2a7c33858ae3853728c1
srv9480 <102672439+srv9480@users.noreply.github.com>
2022-12-23 14:20:54 +0300

    ADD функция отправки запроса на сервер и получения ответа с записью в хранилище состояний

--- src/App.js
+++ src/App.js
@@ -1,4 +1,17 @@
-import './App.css';
+import React, { PureComponent } from "react";
+import { connect } from "react-redux";
+import "./styles.css";
+import { requestDog, requestDogGood, requestDogBad } from "./redux/actions";
+
+const fetchDog = (dispatch) => {
+  dispatch(requestDog());
+  return fetch("https://dog.ceo/api/breeds/image/random")
+    .then((res) => res.json())
+    .then(
+      (data) => dispatch(requestDogGood(data)),
+      (err) => dispatch(requestDogBad())
+    );
+};
 
 function App() {
   return (
